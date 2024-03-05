b47dfc21ef908d2d802b5bc103a5701addc67322
Dafa Hentra <63453455+dafahentra@users.noreply.github.com>
2024-03-05 17:18:52 +0700

    Update About-works.js

--- src/components/About/About-works.js
+++ src/components/About/About-works.js
@@ -8,7 +8,7 @@ export default function AboutWorks() {
 
   return (
     <section id="about" className="about-section" data-scroll-section>
-      <div className="font-white ml-1 tablet-m-0 title title-description"  data-scroll data-scroll-direction="vertical" data-scroll-speed="2">SEBUAH INOVASI HOROR YANG MENGGABUNGKAN ELEMEN RUMAH HANTU KLASIK DENGAN TANTANGAN ESCAPE GAME. KAMI MENCIPTAKAN PENGALAMAN HOROR BARU DAN MEMIKAT BAGI PARA PECINTA PETUALANG</div>
+      <div className="font-white ml-1 tablet-m-0 title title-description"  data-scroll data-scroll-direction="vertical" data-scroll-speed="2">DENGAN PERPADUAN RUMAH HANTU DAN TANTANGAN ESCAPE GAME. KAMI MENCIPTAKAN PENGALAMAN HOROR BARU YANG MEMICU ADRENALIN</div>
       <div className="font-neutral ml-1 mt-24 tablet-ml-0 text"  data-scroll data-scroll-direction="vertical" data-scroll-speed="1">Check out<br></br>my projects &#8594; <a className="font-white hover-underline-animation" href="#works-home">HERE</a></div>
 
       <div className="about-canvas mt-160 ml-4 tablet-ml-0">
