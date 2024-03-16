99acc3afa56ebb5306907de1639409d63f9b18d0
Dafa Hentra <63453455+dafahentra@users.noreply.github.com>
2024-03-16 16:26:15 +0700

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,6 @@ import SeparatorImage from '../components/SeparatorImage/SeparatorImage';
 import AboutInstagram from '../components/About/About-instagram';
 import WorksHome from '../components/Works/WorksHome';
 import Contact from '../components/Contact/Contact';
-import Chapter from '../components/Chapter/Chapter';
 import { useEffect, useState } from 'react';
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
